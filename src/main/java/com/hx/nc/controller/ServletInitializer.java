86315a56ba8bee3ef2cf89ec7bae710386ab9593
xingjjc <xingjjc@yonyou.com>
2019-09-10 10:15:44 +0800

    war包部署

---
+++ src/main/java/com/hx/nc/controller/ServletInitializer.java
@@ -0,0 +1,17 @@
+package com.hx.nc.controller;
+
+import com.hx.nc.ApproveClientAdapterApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author XingJiajun
+ * @Date 2019/9/9 20:36
+ * @Description
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApproveClientAdapterApplication.class);
+    }
+}
